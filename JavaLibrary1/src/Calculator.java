490e7c19c79e63b971f643a8cac4996dff9495d2
Denis_Work\Denis <den58rus@mail.ru>
2017-11-10 10:02:10 +0300

    gf

--- JavaLibrary1/src/Calculator.java
+++ JavaLibrary1/src/Calculator.java
@@ -1,38 +1,25 @@
-// класс реализует калькулятор
-
-
 public class Calculator {
-    
-   /** Результат*/
         private int result;
      
-//математическая операция  сложения String typeOp,
-
     public void add ( String typeOp, int ... params ) {
-        
-        
+                
         for (Integer param : params) {
             System.out.println(param);
+            
             if	(typeOp.equals("+")){
 		this.result += param;
-                }	
-//            if (typeOp.equals("-")) {
-//                this.result = param[i] - param[i-1];
-//            }
-
+                }
             
+            if (typeOp.equals("-")) {
+                this.result = params[0] - params[1];
+            }
         }
-		
     }
-	
-	
-        //вывод результата операции
+        //
         public int otputResult (){
             return this.result;
         }
-        
-        
-     //очистка результата операции   
+     //  
     public void clearResult ()  {
         this.result = 0;
       }
